9744f9e053387ce015e7b141ce840dde7b4235df
Brock Dyer <bld4524@rit.edu>
2022-05-17 18:01:13 -0400

    Remove unwanted files from branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# tiddlywiki-plugins
-A collection of plugins (often focused on D&D campaign management)
--- src/KingdomsAndWarfare/SVGs/process_svg.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sed '/<path.*Text/ s/$/ class="svg-path-text"/' PartySheet.svg > PartySheetHtml.txt
-sed -i '' '/<path.*Banner/ s/$/ class="svg-banner"/' PartySheetHtml.txt
